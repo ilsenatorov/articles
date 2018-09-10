3b981320731f733ed4c93e4a489fdf58cf92b919
ilsenatorov <il.senatorov@gmail.com>
2018-09-10 10:51:42 +0300

    added comments in code (and docstrings)

--- get_all_citations.py
+++ get_all_citations.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+'''
+Main functon that writes down metadata for articles.
+'''
 import mining
 pmids = open('pmids.tsv').read().split()
 for num in range(25, len(pmids), 25):
-    mining.get_25_citedby(pmids[num-25:num])
+    mining.get_25_citedby(pmids[num-25:num], './citations.tsv')
     
--- mining.py
+++ mining.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+New module, used for working with ncbi data.
+'''
 import pandas as pd
 import numpy as np
 import requests
@@ -9,6 +12,9 @@ API = os.environ['ELSKEY']
 
 
 def get_citedby(PMID):
+    '''
+    Get number of citations for an articles based on PMC code. Uses Elsevier API.
+    '''
     try: 
         scopus = requests.get('http://api.elsevier.com/content/search/scopus?query=PMID(%s)&field=citedby-count' % PMID, headers={'X-ELS-APIKEY':API})
         return scopus.json()['search-results']['entry'][0]['citedby-count']
@@ -17,6 +23,9 @@ def get_citedby(PMID):
         return np.nan
 
 def create_scopus_link(pmids):
+    '''
+    Creates a link to make a query about 25 articles (based on PMC codes).
+    '''
     beg = 'http://api.elsevier.com/content/search/scopus?query=PMID(%s)' % pmids[0]
     final = [beg]
     end = '&field=citedby-count'
@@ -24,7 +33,10 @@ def create_scopus_link(pmids):
         final.append('+OR+PMID(%s)' % l)
     return ''.join(final)
 
-def get_25_citedby(pmids):
+def get_25_citedby(pmids, output):
+    '''
+    Records info about 25 articles as a tsv file
+    '''
     link = create_scopus_link(pmids)
     try:
         json = requests.get(link, headers={'X-ELS-APIKEY':API}).json()['search-results']['entry']
@@ -37,7 +49,7 @@ def get_25_citedby(pmids):
             citedby = info['citedby-count']
             title = info['dc:title']
             pubmed = info['pubmed-id']
-            with open('citations.tsv', 'a+') as output:
+            with open(output, 'a+') as output:
                 output.write('%s\t%s\t%s\t%s\t%s\n' % (pmid, date, citedby, title, pubmed))
         except:
             print('Unable to retrieve article with PMID %s' % pmid)
